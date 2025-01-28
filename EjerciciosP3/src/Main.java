cabfea818a89f88fb4687c278cd9377381f62d34
SergioAM25 <123046330@upq.edu.mx>
2025-01-28 14:28:33 -0600

    Ejercicio 1

---
+++ EjerciciosP3/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el numero de horas trabajadas: ");
+        int horas = scanner.nextInt();
+
+        System.out.print("Introduce el coste por hora: ");
+        double costePorHora = scanner.nextDouble();
+
+        double paga = horas * costePorHora;
+        System.out.println("La paga correspondiente es: $" + paga)
+    }
+}
+
+
